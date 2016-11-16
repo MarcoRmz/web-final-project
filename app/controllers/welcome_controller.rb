fc5e94dba278d2a020d5501f188a41ea8a058bb0
Marco <MarcoRmz@users.noreply.github.com>
2016-11-16 10:32:44 -0600

    added announcements to index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+  	@announcements = JSON.parse(File.read("#{Rails.root}" + '/app/assets/javascripts/announcements.json'))
   end
 
   def about_us
